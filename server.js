c14dd2b2dd41c239958bd59b0701eda77aa7efa3
C-Y-P-H-E-R <69472040+C-Y-P-H-E-R@users.noreply.github.com>
2021-07-21 21:24:52 +0530

    completed video chat app !
    Its backend was prepared using Nodejs and frontend was build using Reactjs.
    The data communication is handled by Socket.io !
    Backend Deployed to heroku & frontend deployed to netlify.
    Please feel free to check it out !
    
    kush-video-chat-app.netlify.app
    
    P.S. when running , its voice won't be heard right now as the microphone's are set to be muted.

---
+++ server.js
@@ -0,0 +1,34 @@
+const app = require('express')();
+const cors = require('cors');
+// const { emit } = require('process');
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        method: ["GET","POST"]
+    }
+})
+
+app.use(cors());
+
+const port = process.env.PORT || 5000
+
+app.get("/", (req,res) => {
+    res.send('server is running');
+})
+
+io.on('connection',(socket) => {
+    socket.emit('me',socket.id);
+    socket.on('disconnect',() => {
+        socket.broadcast.emit("callended")
+    }) 
+    socket.on("calluser",({userToCall,signalData, from, name}) => {
+        io.to(userToCall).emit("calluser",{signal: signalData, from, name})
+    })
+    socket.on("answercall",(data)=> {
+        io.to(data.to).emit("callaccepted",data.signal)
+    })
+})
+
+server.listen(port,() => console.log(`server listening on port ${port}`))
